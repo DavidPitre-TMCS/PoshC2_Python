cb5039c5a813fceb6ddab088c6d0be5367028a5b
DavidPitre-TMCS <49197604+DavidPitre-TMCS@users.noreply.github.com>
2019-06-23 16:08:18 +0100

    Update Implant.py
    Replace httplib with python3 equivalent (http.client)
    Update parameter ordering on url for ClockWorkSMS API

--- Implant.py
+++ Implant.py
@@ -73,10 +73,10 @@ IMGS19459394%s49395491SGMI""" % (self.RandomURI, self.AllBeaconURLs, self.KillDa
         try:
 
             if EnableNotifications.lower().strip() == "yes":
-                import httplib, urllib
-                conn = httplib.HTTPSConnection("api.pushover.net:443")
+                import http.client, urllib.request, urllib.parse, urllib.error
+                conn = http.client.HTTPSConnection("api.pushover.net:443")
                 conn.request("POST", "/1/messages.json",
-                             urllib.urlencode({
+                             urllib.parse.urlencode({
                                  "token": APIToken,
                                  "user": APIUser,
                                  "message": "[%s] - NewImplant: %s @ %s" % (NotificationsProjectName, self.User, self.Hostname),
@@ -86,11 +86,11 @@ IMGS19459394%s49395491SGMI""" % (self.RandomURI, self.AllBeaconURLs, self.KillDa
             if EnableNotifications.lower().strip() == "yes" and APIKEY and MobileNumber:
                 for number in MobileNumber.split(","):
                     number = number.replace('"', '')
-                    url = "https://api.clockworksms.com/http/send.aspx?key=%s&to=%s&from=PoshC2&content=[%s]%%20-%%20NewImplant:%%20%s\\%s @ %s" % (NotificationsProjectName, APIKEY, number, self.Domain, self.User, self.Hostname)
+                    url = "https://api.clockworksms.com/http/send.aspx?key=%s&to=%s&from=PoshC2&content=[%s]%%20-%%20NewImplant:%%20%s\\%s @ %s" % (APIKEY, number, NotificationsProjectName, self.Domain, self.User, self.Hostname)
                     url = url.replace(" ", "+")
-                    urllib.urlopen(url)
+                    urllib.request.urlopen(url)
         except Exception as e:
-            print("SMS send error: %s" % e)
+            print(("SMS send error: %s" % e))
 
     def save(self):
         self.ImplantID = new_implant(self.RandomURI, self.User, self.Hostname, self.IPAddress, self.Key, self.FirstSeen, self.FirstSeen, self.PID, self.Proxy, self.Arch, self.Domain, self.Alive, self.Sleep, self.ModsLoaded, self.Pivot, self.Label)
